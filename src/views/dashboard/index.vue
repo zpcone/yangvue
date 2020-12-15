9c30a3fde9c8ad613a07f9b0d7baf6c2dd81d1e2
zhaopengchao <zhaopengchao@17shanyuan.com>
2020-12-15 17:25:42 +0800

    创建文件

---
+++ src/views/dashboard/index.vue
@@ -0,0 +1,186 @@
+<template>
+  <div class="dashboard-editor-container">
+    <el-form :inline="true" :model="formInline" class="demo-form-inline">
+      <el-form-item label="店铺名称">
+        <el-select v-model="formInline.shopName" placeholder="活动区域">
+          <el-option label="jimmy店" value="1"></el-option>
+          <el-option label="机灵点" value="2"></el-option>
+          <el-option label="南宫店" value="3"></el-option>
+          <el-option label="上官店" value="4"></el-option>
+        </el-select>
+      </el-form-item>
+      <el-form-item label="客户姓名">
+        <el-input v-model="formInline.userName" placeholder="姓名"></el-input>
+      </el-form-item>
+      <el-form-item label="旺旺号码">
+        <el-input v-model="formInline.wanghao" placeholder="旺旺号"></el-input>
+      </el-form-item>
+      <el-form-item label="产品">
+        <el-input v-model="formInline.goodsName" placeholder="已购产品"></el-input>
+      </el-form-item>
+   
+      <el-form-item>
+        <el-button plain type="primary" @click="onSubmit">查询</el-button>
+      </el-form-item>
+    </el-form>
+
+
+       <el-table
+        :data="tableData"
+        style="width: 100%">
+        <!-- <el-table-column
+          label="日期"
+          width="180">
+          <template slot-scope="scope">
+            <i class="el-icon-time"></i>
+            <span style="margin-left: 10px">{{ scope.row.date }}</span>
+          </template>
+        </el-table-column> -->
+        <el-table-column
+          type="index"
+          label="序号"
+          width="180">
+        </el-table-column>
+
+        <el-table-column
+          prop="name"
+          label="姓名"
+          width="180">
+        </el-table-column>
+
+        <el-table-column
+          prop="wangNum"
+          label="旺旺账号"
+          width="180">
+        </el-table-column>
+
+       
+
+        <el-table-column
+          label="已购产品"
+          prop="buyGood"
+          width="180">
+        </el-table-column>
+
+        <el-table-column
+          label="售后备注"
+          prop="wait"
+          width="180">
+        </el-table-column>
+
+        <el-table-column label="操作">
+          <template slot-scope="scope">
+            <el-button
+              size="mini"
+              @click="handleEdit(scope.$index, scope.row)">编辑</el-button>
+            <el-button
+              size="mini"
+              type="danger"
+              @click="handleDelete(scope.$index, scope.row)">删除</el-button>
+          </template>
+        </el-table-column>
+      </el-table>
+
+  </div>
+</template>
+
+<script>
+
+
+export default {
+  name: 'DashboardAdmin',
+  data() {
+    return {
+      formInline: {
+        userName: '',
+        wanghao: '',
+        goodsName:'',
+        shopName:''
+      },
+         tableData: [{
+          date: '2016-05-02',
+          name: '王小虎',
+          wangNum:'jdhdghdjklhkljhljlh',
+          address: '上海市普陀区金沙江路 1518 弄',
+          buyGood:'江南系列',
+          wait:'发回北京'
+        }, {
+          date: '2016-05-04',
+          name: '王小虎',
+          wangNum:'jdhdghdjklhkljhljlh',
+          address: '上海市普陀区金沙江路 1517 弄',
+          buyGood:'江南系列',
+          wait:'发回北京'
+        }, {
+          date: '2016-05-01',
+          name: '王小虎',
+          wangNum:'jdhdghdjklhkljhljlh',
+          address: '上海市普陀区金沙江路 1519 弄',
+          buyGood:'江南系列',
+          wait:'发回北京'
+        }, {
+          date: '2016-05-03',
+          name: '王小虎',
+          wangNum:'jdhdghdjklhkljhljlh',
+          address: '上海市普陀区金沙江路 1516 弄',
+          buyGood:'江南系列',
+          wait:'发回北京'
+        }]
+      // lineChartData: lineChartData.newVisitis
+    }
+  },
+  methods: {
+    onSubmit() {
+      if(!this.formInline.shopName){
+        this.$message({
+          message: '请先选择店铺',
+          type: 'warning'
+        });
+      }
+    },
+    handleEdit(index, row) {
+        console.log(index, row);
+        this.$router.push({ path: 'visitorEdit',query:{id:'1'}})
+      },
+      handleDelete(index, row) {
+        var obj = {}
+        obj.username = 'zpc'
+        this.$axios({
+          method:'post',
+          url:'http://192.168.0.102:3000/ceshi',
+          data:obj
+          }).then((response) =>{          //这里使用了ES6的语法
+              console.log(response)       //请求成功返回的数据
+          })
+        console.log(index, row);
+      }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.dashboard-editor-container {
+  padding: 32px;
+  background-color: rgb(240, 242, 245);
+  position: relative;
+
+  .github-corner {
+    position: absolute;
+    top: 0px;
+    border: 0;
+    right: 0;
+  }
+
+  .chart-wrapper {
+    background: #fff;
+    padding: 16px 16px 0;
+    margin-bottom: 32px;
+  }
+}
+
+@media (max-width:1024px) {
+  .chart-wrapper {
+    padding: 8px;
+  }
+}
+</style>
---
+++ src/views/dashboard/visitorEdit.vue
@@ -0,0 +1,104 @@
+<!--  -->
+<template>
+    <div class='' style="margin-top:50px;">
+      <el-form ref="form" :model="sizeForm" label-width="80px">
+        <el-form-item label="店铺名称">
+          <el-select v-model="sizeForm.shopName" style="width:300px;" placeholder="请选择店铺">
+            <el-option label="jimmy店" value="1"></el-option>
+            <el-option label="机灵店" value="2"></el-option>
+            <el-option label="上官店" value="3"></el-option>
+            <el-option label="南宫店" value="4"></el-option>
+          </el-select>
+        </el-form-item>
+        <el-form-item label="旺旺账号">
+          <el-input v-model="sizeForm.wangNum"  style="width:300px" placeholder="请输入旺旺账号"></el-input>
+        </el-form-item>
+        <el-form-item label="已购产品">
+          <el-input v-model="sizeForm.buyGood"  style="width:300px" placeholder="请输入已购产品"></el-input>
+        </el-form-item>
+        <el-form-item label="备注">
+          <el-input v-model="sizeForm.beizhu"  style="width:300px" placeholder="请输入备注信息"></el-input>
+        </el-form-item>
+        <el-form-item label="时间">
+           <el-date-picker
+           style="width:300px"
+            v-model="value2"
+            type="datetime"
+            placeholder="选择日期时间"
+            align="right"
+            :picker-options="pickerOptions">
+          </el-date-picker>
+        </el-form-item>
+        <el-form-item size="large">
+          <el-button type="primary" @click="onSubmit">保存</el-button>
+          <el-button>重置</el-button>
+        </el-form-item>
+      </el-form>
+
+    </div>
+</template>
+
+<script>
+ //api文档自动生成模板
+ /**
+    * @api {get} /user/:id Request User information
+    * @apiName GetUser
+    * @apiGroup User
+    *
+    * @apiParam {Number} id Users unique ID.
+    *
+    * @apiSuccess {String} firstname Firstname of the User.
+    * @apiSuccess {String} lastname  Lastname of the User.
+    */
+    
+export default {
+    
+    components: {},
+    data() {
+        
+        return {
+          sizeForm: {
+            wangNum: '',
+            shopName: '',
+            buyGood: '',
+            beizhu: '',
+            delivery: false,
+            type: [],
+            resource: '',
+            desc: ''
+          }
+            
+        };
+    },
+    //监听属性 类似于data概念
+    computed: {},
+    //监控data中的数据变化
+    watch: {},
+    
+    methods: {
+      onSubmit() {
+        console.log('submit!');
+      }
+    },
+    //生命周期 - 创建完成（可以访问当前this实例）
+    created() {
+        
+    },
+    //生命周期 - 挂载完成（可以访问DOM元素）
+    mounted() {
+      console.log(this.$route.query.id)
+    },
+    beforeCreate() {}, 
+    beforeMount() {},
+    beforeUpdate() {}, 
+    updated() {}, 
+    beforeDestroy() {}, 
+    destroyed() {}, 
+    //如果页面有keep-alive缓存功能，这个函数会触发
+    activated() {}, 
+    }
+</script>
+<style lang='scss' scoped>
+    //@import url(); 引入公共css类
+    
+</style>
